45cc3c315e942d518200675a5d6c6278c5df8e1e
makodaa <qmkbcerenio@tip.edu.ph>
2025-04-07 02:39:51 +0800

    Add SecurityQuestionsDao for managing security questions in the database

---
+++ lib/data/database/dao/security_questions_dao.dart
@@ -0,0 +1,89 @@
+import 'package:easthardware_pms/data/database/database_helper.dart';
+import 'package:easthardware_pms/data/database/tables/security_questions_table.dart';
+import 'package:easthardware_pms/domain/models/security_question.dart';
+
+abstract class SecurityQuestionsDao {
+  Future<List<SecurityQuestion?>> getAllSecurityQuestions();
+  Future<SecurityQuestion?> getSecurityQuestionById(int id);
+  Future<List<SecurityQuestion?>> getSecurityQuestionsByUserId(int id);
+  Future<SecurityQuestion> insertSecurityQuestion(SecurityQuestion securityQuestion);
+  Future<SecurityQuestion> updateSecurityQuestion(SecurityQuestion securityQuestion);
+  Future<void> deleteSecurityQuestion(int id);
+}
+
+class SecurityQuestionsDaoImpl extends SecurityQuestionsDao {
+  final DatabaseHelper _databaseHelper;
+  SecurityQuestionsDaoImpl([DatabaseHelper? databaseHelper])
+      : _databaseHelper = databaseHelper ?? DatabaseHelper();
+
+  @override
+  Future<void> deleteSecurityQuestion(int id) async {
+    final database = await _databaseHelper.database;
+    await database.delete(
+      SecurityQuestionsTable.SECURITY_QUESTIONS_TABLE,
+      where: '${SecurityQuestionsTable.SECURITY_QUESTIONS_ID} = ?',
+      whereArgs: [id],
+    );
+  }
+
+  @override
+  Future<List<SecurityQuestion?>> getAllSecurityQuestions() async {
+    final database = await _databaseHelper.database;
+    var queryResults = await database.query(SecurityQuestionsTable.SECURITY_QUESTIONS_TABLE);
+
+    return queryResults.map(SecurityQuestion.fromMap).toList();
+  }
+
+  @override
+  Future<SecurityQuestion?> getSecurityQuestionById(int id) async {
+    final database = await _databaseHelper.database;
+    var queryResults = await database.query(
+      SecurityQuestionsTable.SECURITY_QUESTIONS_TABLE,
+      where: '${SecurityQuestionsTable.SECURITY_QUESTIONS_ID} = ?',
+      whereArgs: [id],
+    );
+
+    try {
+      var json = queryResults.single;
+
+      return SecurityQuestion.fromMap(json);
+    } on StateError {
+      return null;
+    }
+  }
+
+  @override
+  Future<SecurityQuestion> insertSecurityQuestion(SecurityQuestion securityQuestion) async {
+    final database = await _databaseHelper.database;
+    await database.insert(
+      SecurityQuestionsTable.SECURITY_QUESTIONS_TABLE,
+      securityQuestion.toMap(),
+    );
+
+    return securityQuestion;
+  }
+
+  @override
+  Future<SecurityQuestion> updateSecurityQuestion(SecurityQuestion securityQuestion) async {
+    final database = await _databaseHelper.database;
+    await database.update(
+      SecurityQuestionsTable.SECURITY_QUESTIONS_TABLE,
+      securityQuestion.toMap(),
+      where: '${SecurityQuestionsTable.SECURITY_QUESTIONS_ID} = ?',
+      whereArgs: [securityQuestion.id],
+    );
+
+    return securityQuestion;
+  }
+
+  @override
+  Future<List<SecurityQuestion?>> getSecurityQuestionsByUserId(int id) async {
+    final database = await _databaseHelper.database;
+    var queryResults = await database.query(
+      SecurityQuestionsTable.SECURITY_QUESTIONS_TABLE,
+      where: '${SecurityQuestionsTable.SECURITY_QUESTIONS_USER_ID} = ?',
+      whereArgs: [id],
+    );
+    return queryResults.map(SecurityQuestion.fromMap).toList();
+  }
+}
